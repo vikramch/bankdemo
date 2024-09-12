347f707055df00e3620b22c1d63c59eee113d441
vikramch <chunduruvikram@gmail.com>
2024-09-12 18:26:55 +1000

    Init commit (#1)
    * Adding git ignore
    
    * Initial bankdemo commit from Vikram for Macquarie
    
    * cleaning up pom
    
    * Setting basic entities, models and H2 DB integration
    
    * Adding responseentity to web controller
    
    * Read me file

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # bankdemo
->Spring Boot app to support a web application that allows an user to view transactions on any of the accounts they hold.
+Spring Boot app to support a web application that allows an user to view transactions on any of the accounts they hold.
---
+++ src/main/java/com/vikram/macquarie/bankdemo/controller/AccountController.java
@@ -0,0 +1,31 @@
+package com.vikram.macquarie.bankdemo.controller;
+
+import com.vikram.macquarie.bankdemo.domain.model.Account;
+import com.vikram.macquarie.bankdemo.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountController {
+
+    @Autowired
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Account>> getAccountList() {
+
+        List<Account> accounts = accountService.fetchAllAccounts();
+        return new ResponseEntity<List<Account>>(accounts, HttpStatus.OK);
+    }
+}
