e002c16e627f9ebb40b40dc0ec44b39cc9c8fcb9
Vikram <vikrambpgc@gmail.com>
2024-09-12 14:18:22 +1000

    Initial bankdemo commit from Vikram for Macquarie

---
+++ src/test/java/com/vikram/macquarie/bankdemo/BankdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikram.macquarie.bankdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
